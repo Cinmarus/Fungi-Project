a2eb34f7d6cbfbce9e962fc3d505ca54c18aa1b0
Daris <70913837+DarisBerzins@users.noreply.github.com>
2025-03-17 12:28:44 +0100

    not working spectrogram

--- test.py
+++ test.py
@@ -1,35 +1,59 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# test = pd.read_csv("data/data_unshielded.csv")
-# test.to_pickle("df")
-
-test = pd.read_pickle("df")
-print(test.head())
-
-channels = [test["Ch_1"], test["Ch_2"], test["Ch_3"], test["Ch_4"]]
-
-transformedChannels = []
-for channel in channels:
-    transformedChannels.append(np.fft.fft(channel))
-
-sampleTime = 60 #ms
-dataGap = 60/1000 #s
-
-xs = np.fft.fftfreq(len(transformedChannels[0]), d=dataGap)
+import matplotlib
+import os
 
 
-sine_freq = 10  # Choose your desired frequency in Hz
-t = np.arange(0, len(transformedChannels[0]) * dataGap, dataGap)  # Time vector
-sine_wave = np.sin(2 * np.pi * sine_freq * t)*4  # Generate sine wave
+# matplotlib.use('Agg')
+plt.rcParams['agg.path.chunksize'] = 100000000
+# plt.rcParams['path.simplify'] = True
+# plt.rcParams['path.simplify_threshold'] = 0.1
 
-# Compute FFT of sine wave
-sine_fft = np.fft.fft(sine_wave)
 
-for channel in transformedChannels:
-    plt.plot(xs, np.abs(channel))
+data = pd.read_csv("data/new_data.csv")
 
-plt.plot(xs, np.abs(sine_fft), 'k--', label=f"Sine {sine_freq} Hz", linewidth=2)
+if os.path.isfile('data/data_pickled'):
+    data = pd.read_pickle("data/data_pickled")
+    print("unpickled")
+else:
+    data.to_pickle("data/data_pickled")
+    print("pickled")
+
+
+
+
+
+
+
+data["Rolling Average"] = data["Voltage"].rolling(window=100, min_periods=0).mean()
+data["Baseline"] = data["Voltage"].rolling(window=50000, min_periods=0).mean()
+data["Flattened"] = data["Rolling Average"] - data["Baseline"]
+
+
+print(data.head())
+
+# plt.plot(data["Voltage"])
+# plt.plot(data["Rolling Average"])
+# plt.plot(data["Baseline"])
+# plt.plot(data["Flattened"])
+# plt.show()
+
+
+NFFT = 65536  # the length of the windowing segments
+Fs = 100/6  # the sampling frequency
+
+fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
+ax1.plot(data["Voltage"])
+ax1.set_ylabel('Signal')
+
+Pxx, freqs, bins, im = ax2.specgram(data["Voltage"], NFFT=NFFT, noverlap=NFFT//2, Fs=Fs)
+# The `specgram` method returns 4 objects. They are:
+# - Pxx: the periodogram
+# - freqs: the frequency vector
+# - bins: the centers of the time bins
+# - im: the .image.AxesImage instance representing the data in the plot
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel('Frequency (Hz)')
 
 plt.show()
