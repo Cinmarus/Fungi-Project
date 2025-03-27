d7f57a0403c796e8fbbb1d647bb1a8206a8ed780
Daris <70913837+DarisBerzins@users.noreply.github.com>
2025-03-27 12:30:32 +0100

    idk what i did anymore but i think i fiddled with some stuff

--- spectrogram.py
+++ spectrogram.py
@@ -3,8 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import os
-from scipy import signal
-
+from scipy.signal import spectrogram
 
 
 if os.path.isfile('data/data_pickled'):
@@ -23,9 +22,9 @@ else:
 
 data["Time"] -= data["Time"].iloc[0]
 
-data["Rolling Average"] = data["Voltage"].rolling(window=100, min_periods=0, center=True).mean()
-data["Baseline"] = data["Voltage"].rolling(window=50000, min_periods=0, center=True).mean()
-data["Flattened"] = data["Rolling Average"] - data["Baseline"]
+data["Rolling Average"] = data["Voltage"].rolling(window=5000, min_periods=0, center=True).mean()
+data["Baseline"] = data["Voltage"].rolling(window=5000, min_periods=0, center=True).mean()
+data["Flattened"] = data["Voltage"] - data["Baseline"]
 
 time = data["Time"]
 voltage = data["Voltage"]
@@ -35,14 +34,14 @@ flattened = data["Flattened"]
 
 print(data.head())
 
-plt.plot(data["Voltage"])
-plt.plot(data["Rolling Average"])
-plt.plot(data["Baseline"])
-plt.plot(data["Flattened"])
-plt.show()
+# plt.plot(data["Voltage"])
+# plt.plot(data["Rolling Average"])
+# plt.plot(data["Baseline"])
+# plt.plot(data["Flattened"])
+# plt.show()
 
 
-def plotSpectrogram(time, signal):
+def plotSpectrogram(time, signal, title):
 
     totalTime = time.iloc[-1]-time.iloc[0]
     Fs = len(time)/totalTime    
@@ -56,20 +55,22 @@ def plotSpectrogram(time, signal):
     ax1 = fig.add_subplot(gs[0, 1]) #signal plot
     ax2 = fig.add_subplot(gs[1:, 1]) # spectrogram
     ax3 = fig.add_subplot(gs[1:, 0]) # fourier transform
+    # textax = fig.add_subplot(gs[0, 0])
         
+    # textax.text(0, 0, title)
+    # textax.axis('off')
+
     ax1.plot(time, signal)
     ax1.set_xlim([0, time.iloc[-1]])
-    ax1.set_ylabel('Signal')
+    ax1.set_ylabel(r'Voltage [$\mu$V]')
 
     Pxx, freq, t = matplotlib.mlab.specgram(signal, NFFT=NFFT, noverlap=NFFT*3//4, Fs=Fs)
 
     Pxx_log = 10 * np.log10(Pxx + 1e-10)
     pcm = ax2.pcolormesh(t, freq, Pxx_log, cmap='plasma', shading='auto', vmin=np.percentile(Pxx_log, 1), vmax=np.percentile(Pxx_log, 99))
-    fig.colorbar(pcm, ax=ax2)
-    # ax2.set_yscale('log')
-
+    cb = fig.colorbar(pcm, ax=ax2)
+    cb.set_label('Amplitude')
     ax2.set_xlabel('Time (s)')
-    # ax2.set_ylabel('Frequency (Hz)')
     ax2.set_xlim([0, time.iloc[-1]])
 
     yf = np.fft.rfft(signal)
@@ -79,7 +80,8 @@ def plotSpectrogram(time, signal):
     ax3.set_yscale('log')
 
     ax3.plot(yf, xf)
-    # ax3.set_xlim([None, None])
+    ax3.set_xlim([1e3, None])
+    ax3.set_ylim([1e-4, Fs/2])
     ax2.set_ylim(ax3.get_ylim())
 
     ax3.set_ylabel('Frequency (Hz)')
@@ -93,6 +95,6 @@ def plotSpectrogram(time, signal):
     plt.show()
 
 # plotSpectrogram(time, voltage)
-# plotSpectrogram(time, rollingAverage)
-# plotSpectrogram(time, flattened)
+# plotSpectrogram(time, rollingAverage, "Rolling Average Spectrogram")
+plotSpectrogram(time, flattened, "Flattened Data")
 # plotSpectrogram(time, baseline)
