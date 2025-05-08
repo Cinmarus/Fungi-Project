d0ebbb5721c0f1b3cde197910b534a7e60e50fe9
Marco <marco.armeli04@gmail.com>
2025-05-08 10:30:13 +0200

    update

--- spike_detection.py
+++ spike_detection.py
@@ -165,6 +165,7 @@ class peak_analyser:
         bins = round((max_val - min_val) / bin_width)
         bins2 = (max_val - min_val) / bin_width
         bin_width2 = (max_val - min_val)/bins 
+        print(len(peak_amplitudes))
         print(bins)
         print(bins2)
         print(f"Bin Width for Amplitude: {bin_width:.4f} µV")
