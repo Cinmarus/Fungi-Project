c9033cc9c36b94e8452b62d359aa7cea97c03e29
Bart Flikweert <flikweert.bart@gmail.com>
2025-05-15 12:18:28 +0200

    Added mclaren colorway

--- Data_visualisation.py
+++ Data_visualisation.py
@@ -10,7 +10,7 @@ def set_plt_defaults(font_size=12):
     """
     # Set the default color cycle to start with light grey and orange
     plt.rcParams['axes.prop_cycle'] = plt.cycler('color', 
-                                                ['#b8b8b8', '#1a80bb'])
+                                                ['#47c7fc', '#ff8000'])
     
     # Set font sizes
     plt.rcParams['font.size'] = font_size
